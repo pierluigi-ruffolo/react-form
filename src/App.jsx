27f8ce4e95dc593daab7591f9c01f06c5f0cf6bf
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-01 14:32:51 +0100

    feat: Implementata Milestone 1 Lista Articoli

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  const [articles, setArticles] = useState([
+    "Le cinque app che cambieranno il tuo modo di fare la spesa",
+    "Ritorno alla Luna: la nuova corsa allo spazio e le sue implicazioni",
+  ]);
+
+  return (
+    <>
+      <header>
+        <h1>Gestione Articoli</h1>
+      </header>
+      <main>
+        <div className="container">
+          <ul>
+            {articles.map((article) => (
+              <li>{article}</li>
+            ))}
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+}
 
 export default App;
