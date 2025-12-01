ebd75a7df6f83de03fa64c697145648037d5fac6
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-01 15:09:16 +0100

    feat:  form per aggiungere   articoli

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,16 @@ function App() {
     "Le cinque app che cambieranno il tuo modo di fare la spesa",
     "Ritorno alla Luna: la nuova corsa allo spazio e le sue implicazioni",
   ]);
+  const [inputValue, SetInputValue] = useState("");
 
+  const handleAddArticle = (event) => {
+    event.preventDefault();
+    if (inputValue === "") {
+      return console.log("inserisci un titolo");
+    } else {
+      setArticles([inputValue, ...articles]);
+    }
+  };
   return (
     <>
       <header>
@@ -14,10 +23,21 @@ function App() {
       </header>
       <main>
         <div className="container">
+          <h2>Aggiungi un nuovo articolo</h2>
+          <form onSubmit={handleAddArticle}>
+            <input
+              type="text"
+              placeholder="Inserisci articolo..."
+              value={inputValue}
+              onChange={(e) => SetInputValue(e.target.value)}
+            />
+            <button type="submit">Aggiungi</button>
+          </form>
+
           <ul>
-            {articles.map((article) => (
-              <li>{article}</li>
-            ))}
+            {articles.map((article, index) => {
+              return <li key={index}>{article}</li>;
+            })}
           </ul>
         </div>
       </main>
